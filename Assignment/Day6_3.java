9da9bdbca896d0609072172a5a2addb40d3705d6
Sujit Sampat Sakore <150163338+SujitSakore@users.noreply.github.com>
2023-12-27 00:17:40 +0530

    Add files via upload

---
+++ Assignment/Day6_3.java
@@ -0,0 +1,10 @@
+//Create a class which compares the content of two command line arguments and display whether they are equal or not.
+
+
+class command3
+{
+    public static void main(String args[])
+    {
+        System.out.println(args[0].equals(args[1]));
+    }
+}
