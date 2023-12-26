ead594ef635c786b901fef34796ae4d584c8d0fa
Sujit Sampat Sakore <150163338+SujitSakore@users.noreply.github.com>
2023-12-27 00:18:05 +0530

    Add files via upload

---
+++ Assignment/Day6_4.java
@@ -0,0 +1,23 @@
+//Write a program to check the use of "= =" operator using String.
+
+
+class Day6_4
+{
+	public static void main(String args[])
+	{
+	if(args[0].equals(args[1]))
+	{
+	System.out.println("String are same");
+	}
+	else{
+	System.out.println("String are not same");
+	}
+	if(args[0]==args[1])
+	{
+	System.out.println("String are same");
+	}
+	else{
+	System.out.println("String are not same");
+	}
+	}
+}
